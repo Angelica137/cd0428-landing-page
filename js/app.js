dc5d4d719a54527c0fec06c0be5843c6cbaba207
Angelica <Angelica137@users.noreply.github.com>
2024-01-12 17:29:59 +0000

    add helper function to create nav links and appned to the nav

--- js/app.js
+++ js/app.js
@@ -30,7 +30,17 @@ const navBar = document.getElementById("navBar");
  * Start Helper Functions
  *
  */
+function createNavLinks(section) {
+  const li = documnet.createElement("li");
+  const a = documnent.createElement("a");
+  a.textContent = section.querySelctor("h2").textContent;
+  a.href = "#" + section.id;
 
+  li.appendChild(a);
+  URL.appendChild(li);
+}
+
+navBar.appendChild(ul);
 /**
  * End Helper Functions
  * Begin Main Functions
